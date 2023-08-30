a8870fdafa182039463f187ef60967264fac4cb3
HuseinHQ <mhusein463@gmail.com>
2023-08-30 14:26:29 +0700

    Membuat dom untuk render card

---
+++ index.js
@@ -0,0 +1,65 @@
+let datas = [
+  {
+    nama: "Soekarno",
+    tempatLahir: "Surabaya",
+    tanggalLahir: "6 Juni 1901",
+    meninggal: "21 Juni 1970",
+    img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/02/Soekarno_sq-1-e1585486258865.jpg",
+    desc: 'Soekarno, dikenal juga sebagai Bung Karno, adalah salah satu tokoh utama dalam perjuangan kemerdekaan Indonesia. Ia lahir pada tahun 1901 dan menjadi proklamator kemerdekaan Indonesia pada 1945, serta presiden pertama negara ini hingga tahun 1967. Soekarno dikenal sebagai orator ulung dan pemimpin karismatik yang memimpin rakyat Indonesia melalui perjuangan berat dalam meraih kemerdekaan dari penjajahan Belanda. Ia juga merupakan pendiri ideologi nasional "Pancasila" dan mengadvokasi gerakan non-blok selama periode Perang Dingin. Meskipun pemerintahannya berakhir dengan pergantian kekuasaan, warisan dan pengaruhnya dalam sejarah modern Indonesia tetap dikenang hingga kini.',
+  },
+  {
+    nama: "Soekarno",
+    tempatLahir: "Surabaya",
+    tanggalLahir: "6 Juni 1901",
+    meninggal: "21 Juni 1970",
+    img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/02/Soekarno_sq-1-e1585486258865.jpg",
+    desc: 'Soekarno, dikenal juga sebagai Bung Karno, adalah salah satu tokoh utama dalam perjuangan kemerdekaan Indonesia. Ia lahir pada tahun 1901 dan menjadi proklamator kemerdekaan Indonesia pada 1945, serta presiden pertama negara ini hingga tahun 1967. Soekarno dikenal sebagai orator ulung dan pemimpin karismatik yang memimpin rakyat Indonesia melalui perjuangan berat dalam meraih kemerdekaan dari penjajahan Belanda. Ia juga merupakan pendiri ideologi nasional "Pancasila" dan mengadvokasi gerakan non-blok selama periode Perang Dingin. Meskipun pemerintahannya berakhir dengan pergantian kekuasaan, warisan dan pengaruhnya dalam sejarah modern Indonesia tetap dikenang hingga kini.',
+  },
+  {
+    nama: "Soekarno",
+    tempatLahir: "Surabaya",
+    tanggalLahir: "6 Juni 1901",
+    meninggal: "21 Juni 1970",
+    img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/02/Soekarno_sq-1-e1585486258865.jpg",
+    desc: 'Soekarno, dikenal juga sebagai Bung Karno, adalah salah satu tokoh utama dalam perjuangan kemerdekaan Indonesia. Ia lahir pada tahun 1901 dan menjadi proklamator kemerdekaan Indonesia pada 1945, serta presiden pertama negara ini hingga tahun 1967. Soekarno dikenal sebagai orator ulung dan pemimpin karismatik yang memimpin rakyat Indonesia melalui perjuangan berat dalam meraih kemerdekaan dari penjajahan Belanda. Ia juga merupakan pendiri ideologi nasional "Pancasila" dan mengadvokasi gerakan non-blok selama periode Perang Dingin. Meskipun pemerintahannya berakhir dengan pergantian kekuasaan, warisan dan pengaruhnya dalam sejarah modern Indonesia tetap dikenang hingga kini.',
+  },
+];
+
+const container = document.querySelector(".container");
+
+for (let data of datas) {
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+
+  const cardImg = document.createElement("div");
+  cardImg.setAttribute("class", "card-img");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", data.img);
+  cardImg.appendChild(img);
+
+  const cardContent = document.createElement("div");
+  cardContent.setAttribute("class", "card-content");
+
+  const h3 = document.createElement("h3");
+  h3.innerText = data.nama;
+  const p1 = document.createElement("p");
+  p1.innerHTML = `<span>Tempat lahir:</span> ${data.tempatLahir}`;
+  const p2 = document.createElement("p");
+  p2.innerHTML = `<span>Tanggal lahir:</span> ${data.tanggalLahir}`;
+  const p3 = document.createElement("p");
+  p3.innerHTML = `<span>Meninggal:</span> ${data.meninggal}`;
+  const p4 = document.createElement("p");
+  p4.innerText = data.desc;
+
+  cardContent.appendChild(h3);
+  cardContent.appendChild(p1);
+  cardContent.appendChild(p2);
+  cardContent.appendChild(p3);
+  cardContent.appendChild(p4);
+
+  card.appendChild(cardImg);
+  card.appendChild(cardContent);
+  //   console.log(card);
+  container.appendChild(card);
+}
