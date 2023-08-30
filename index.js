876fd63e40d7cc661553ef4d3afd8f4e38b59caa
HuseinHQ <mhusein463@gmail.com>
2023-08-30 14:57:19 +0700

    Fix bug render card

--- index.js
+++ index.js
@@ -23,11 +23,39 @@ let datas = [
     img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/02/Soekarno_sq-1-e1585486258865.jpg",
     desc: 'Soekarno, dikenal juga sebagai Bung Karno, adalah salah satu tokoh utama dalam perjuangan kemerdekaan Indonesia. Ia lahir pada tahun 1901 dan menjadi proklamator kemerdekaan Indonesia pada 1945, serta presiden pertama negara ini hingga tahun 1967. Soekarno dikenal sebagai orator ulung dan pemimpin karismatik yang memimpin rakyat Indonesia melalui perjuangan berat dalam meraih kemerdekaan dari penjajahan Belanda. Ia juga merupakan pendiri ideologi nasional "Pancasila" dan mengadvokasi gerakan non-blok selama periode Perang Dingin. Meskipun pemerintahannya berakhir dengan pergantian kekuasaan, warisan dan pengaruhnya dalam sejarah modern Indonesia tetap dikenang hingga kini.',
   },
+  {
+    nama: "Soekarno",
+    tempatLahir: "Surabaya",
+    tanggalLahir: "6 Juni 1901",
+    meninggal: "21 Juni 1970",
+    img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/02/Soekarno_sq-1-e1585486258865.jpg",
+    desc: 'Soekarno, dikenal juga sebagai Bung Karno, adalah salah satu tokoh utama dalam perjuangan kemerdekaan Indonesia. Ia lahir pada tahun 1901 dan menjadi proklamator kemerdekaan Indonesia pada 1945, serta presiden pertama negara ini hingga tahun 1967. Soekarno dikenal sebagai orator ulung dan pemimpin karismatik yang memimpin rakyat Indonesia melalui perjuangan berat dalam meraih kemerdekaan dari penjajahan Belanda. Ia juga merupakan pendiri ideologi nasional "Pancasila" dan mengadvokasi gerakan non-blok selama periode Perang Dingin. Meskipun pemerintahannya berakhir dengan pergantian kekuasaan, warisan dan pengaruhnya dalam sejarah modern Indonesia tetap dikenang hingga kini.',
+  },
+  {
+    nama: "Soekarno",
+    tempatLahir: "Surabaya",
+    tanggalLahir: "6 Juni 1901",
+    meninggal: "21 Juni 1970",
+    img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/02/Soekarno_sq-1-e1585486258865.jpg",
+    desc: 'Soekarno, dikenal juga sebagai Bung Karno, adalah salah satu tokoh utama dalam perjuangan kemerdekaan Indonesia. Ia lahir pada tahun 1901 dan menjadi proklamator kemerdekaan Indonesia pada 1945, serta presiden pertama negara ini hingga tahun 1967. Soekarno dikenal sebagai orator ulung dan pemimpin karismatik yang memimpin rakyat Indonesia melalui perjuangan berat dalam meraih kemerdekaan dari penjajahan Belanda. Ia juga merupakan pendiri ideologi nasional "Pancasila" dan mengadvokasi gerakan non-blok selama periode Perang Dingin. Meskipun pemerintahannya berakhir dengan pergantian kekuasaan, warisan dan pengaruhnya dalam sejarah modern Indonesia tetap dikenang hingga kini.',
+  },
+  {
+    nama: "Soekarno",
+    tempatLahir: "Surabaya",
+    tanggalLahir: "6 Juni 1901",
+    meninggal: "21 Juni 1970",
+    img: "https://d22gwcrfo2de51.cloudfront.net/wp-content/uploads/2020/02/Soekarno_sq-1-e1585486258865.jpg",
+    desc: 'Soekarno, dikenal juga sebagai Bung Karno, adalah salah satu tokoh utama dalam perjuangan kemerdekaan Indonesia. Ia lahir pada tahun 1901 dan menjadi proklamator kemerdekaan Indonesia pada 1945, serta presiden pertama negara ini hingga tahun 1967. Soekarno dikenal sebagai orator ulung dan pemimpin karismatik yang memimpin rakyat Indonesia melalui perjuangan berat dalam meraih kemerdekaan dari penjajahan Belanda. Ia juga merupakan pendiri ideologi nasional "Pancasila" dan mengadvokasi gerakan non-blok selama periode Perang Dingin. Meskipun pemerintahannya berakhir dengan pergantian kekuasaan, warisan dan pengaruhnya dalam sejarah modern Indonesia tetap dikenang hingga kini.',
+  },
 ];
 
-const container = document.querySelector(".container");
+const gallery = document.querySelector("#gallery");
+let container = document.createElement("div");
+container.setAttribute("class", "container");
+let count = 0;
 
 for (let data of datas) {
+  count++;
   const card = document.createElement("div");
   card.setAttribute("class", "card");
 
@@ -60,6 +88,14 @@ for (let data of datas) {
 
   card.appendChild(cardImg);
   card.appendChild(cardContent);
-  //   console.log(card);
+
   container.appendChild(card);
+
+  if (count % 3 === 0) {
+    gallery.appendChild(container);
+    container = document.createElement("div");
+    container.setAttribute("class", "container");
+  }
 }
+
+gallery.appendChild(container);
